3e70179cd0b4c575ee25a5355a5d021a4782246f
Italo <italocmelo_@hotmail.com>
2019-09-30 14:01:25 -0300

    estilo da lista

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text , FlatList} from 'react-native';
+import { View, Text, FlatList , StyleSheet } from 'react-native';
 import webservice from '../services/webservice'
+import { white } from 'ansi-colors';
 
 export default class Main extends Component {
 
@@ -15,31 +16,49 @@ export default class Main extends Component {
     loadMyRepos = async () => {
         const response = webservice.get('/users/Italord0/repos')
             .then(response => {
-                //console.log(response.data);
                 const repos = response.data;
-                this.setState( {repos} );
+                this.setState({ repos });
             })
             .catch(error => {
                 console.log(error);
             });
     }
 
-    renderItem = ( {item} ) =>(
-        <View>
-            <Text>{item.name}</Text>
-            <Text>{item.description}</Text>
+    renderItem = ({ item }) => (
+        <View style={styles.container}>
+            <Text style={styles.title}>{item.name}</Text>
+            <Text style={styles.description}>{item.description}</Text>
         </View>
     );
 
     render() {
         return (
             <View>
-                <FlatList 
-                data={this.state.repos}
-                keyExtractor={item => item.id}
-                renderItem={this.renderItem}
+                <FlatList
+                    data={this.state.repos}
+                    keyExtractor={item => item.id}
+                    renderItem={this.renderItem}
                 />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#53418C",
+        margin: 8,
+        padding: 16,
+        borderRadius: 8,
+    },
+    title : {
+        fontSize : 40,
+        color: 'white',
+    },
+    description: {
+        marginTop: 30,
+        fontSize : 20,
+        color: '#FAFBFC'
+    }
+
+});
